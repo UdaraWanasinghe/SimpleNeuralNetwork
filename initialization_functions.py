39b86f6c1950d589aa3a4859191f0f443a2bee51
UdaraWanasinghe <udara.developer@gmail.com>
2020-07-29 14:32:10 +0530

    added initialization functions

---
+++ initialization_functions.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def zero_initialize(sizes):
+    """Zero initializes all weights and biases. Note that
+    this is not the best practise to follow"""
+    weights = [np.zeros((x, y))
+               for x, y in zip(sizes[1:], sizes[:-1])]
+    biases = [np.zeros((x, 1)) for x in sizes[1:]]
+    return (weights, biases)
+
+
+def random_initialize(sizes):
+    """initialize randomly using Gaussian distribution with mean 0 and variance 1"""
+    weights = [np.random.randn((x, y))
+               for x, y in zip(sizes[1:], sizes[:-1])]
+    biases = [np.random.randn(x, 1) for x in sizes[1:]]
+    return (weights, biases)
